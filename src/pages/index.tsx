f45fa9ce4b0fb07524b3dd7a4f7a29b93c30bda5
fabioNog <fabio.nogueira.carmo@gmail.com>
2022-08-24 20:22:10 -0300

    Insert tailwind and add @ types css and config index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center      
+      `}>
+      Texto
+    </div>
+  )
+}
+
+export default Home
