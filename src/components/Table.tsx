7ff06a5396cb3408a21028fcd019ddba95dad175
fabioNog <fabio.nogueira.carmo@gmail.com>
2022-08-25 17:18:15 -0300

    Insert css into table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -10,10 +10,10 @@ export const Table:React.FC<ITableProps> = (props) => {
     function RenderHeader(){
         return (
             <tr>
-                <th>Fazendeiro</th>
-                <th>Fazenda</th>
-                <th>Cidade</th>
-                <th>Data</th>
+                <th className="text-left p-4">Fazendeiro</th>
+                <th className="text-left p-4">Fazenda</th>
+                <th className="text-left p-4">Cidade</th>
+                <th className="text-left p-4">Data</th>
             </tr>
         )
     }
@@ -21,22 +21,26 @@ export const Table:React.FC<ITableProps> = (props) => {
     function RenderData(){
         return props.checklist?.map((checklist,i) => {
             return (
-                <tr key={checklist.id}>
-                    <td>{checklist.id}</td>
-                    <td>{checklist.nameFarm}</td>
-                    <td>{checklist.cityFarm}</td>
+                <tr key={checklist.id} className={`
+                    ${i % 2 === 0 ? 'bg-orange-200': 'bg-orange-100'}
+                `}>
+                    <td >{checklist.id}</td>
+                    <td className="text-left p-4">{checklist.nameFarm}</td>
+                    <td className="text-left p-4">{checklist.cityFarm}</td>
+                    <td className="text-left p-4">24/5/2022</td>
                 </tr>
             )
         })
     }
 
     return (
-        <table className="w-full">
+        <table className="w-full rounded-xl overflow-hidden">
             <thead className={`
                 bg-gradient-to-r 
                 from-orange-500
                 to-orange-800
                 text-gray-100
+                
             `}>
                 {RenderHeader()}
             </thead>
