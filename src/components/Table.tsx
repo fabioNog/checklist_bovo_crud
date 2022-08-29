666815a62b5c5fd3b555211b9e6e3d791087ee41
fabioNog <fabio.nogueira.carmo@gmail.com>
2022-08-29 08:47:18 -0300

    Fixed interface for table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,61 +1,63 @@
-import {useCallback, useEffect, useState} from 'react'
-
-import CheckList from "../core/Checklist"
-
-
-interface ITableProps{
-    checklist: Array<object>;
-}
-
-
-export const Table:React.FC<ITableProps> = (props) => {    
-
-    const [checklistState,setChecklistState] = useState(props.checklist)
-    
-    
-    function RenderHeader(){
-        return (
-            <tr>
-                <th className="text-left p-4">Fazendeiro</th>
-                <th className="text-left p-4">Fazenda</th>
-                <th className="text-left p-4">Cidade</th>
-                <th className="text-left p-4">Data</th>
-            </tr>
-        )
+type CheckListType = {
+    _id: number
+    type: string
+    created_at: string
+    from: {
+      name: string
     }
-    
-    //console.log(checklistState)
-
-    function RenderData(){
-        return props.checklist?.map((checklist,i) => {
-            return (
-                <tr key={checklist._id} className={`
-                    ${i % 2 === 0 ? 'bg-orange-200': 'bg-orange-100'}
-                `}>
-                    <td >{checklist.from.name}</td>
-                    <td className="text-left p-4">{checklist.farmer.name}</td>
-                    <td className="text-left p-4">{checklist.farmer.city}</td>
-                    <td className="text-left p-4">{checklist.created_at}</td>
-                </tr>
-            )
-        })
-           
+    farmer: {
+      name: string
+      city: string
     }
-
-    return (
-        <table className="w-full rounded-xl overflow-hidden">
-            <thead className={`
-                bg-gradient-to-r 
-                from-orange-500
-                to-orange-800
-                text-gray-100
-                
-            `}>
-                {RenderHeader()}
-            </thead>
-            <tbody>
-                {RenderData()}
-            </tbody>
-        </table>
-    )
-} 
+  }
+  
+  type TableProps = {
+      checklist: CheckListType[];
+  }
+  
+  export const Table = ({ checklist }: TableProps) => {
+      function RenderHeader(){
+          return (
+              <tr>
+                  <th className="text-left p-4">Fazendeiro</th>
+                  <th className="text-left p-4">Fazenda</th>
+                  <th className="text-left p-4">Cidade</th>
+                  <th className="text-left p-4">Data</th>
+              </tr>
+          )
+      }
+  console.log(checklist)
+      function RenderData(){
+          return checklist?.map((checklist, i) => {
+              return (
+                  <tr key={checklist._id} className={`
+                      ${i % 2 === 0 ? 'bg-orange-200': 'bg-orange-100'}
+                  `}>
+                      <td >{checklist.from.name}</td>
+                      <td className="text-left p-4">{checklist.farmer.name}</td>
+                      <td className="text-left p-4">{checklist.farmer.city}</td>
+                      <td className="text-left p-4">{checklist.created_at}</td>
+                  </tr>
+              )
+          })
+  
+      }
+  
+      return (
+          <table className="w-full rounded-xl overflow-hidden">
+              <thead className={`
+                  bg-gradient-to-r 
+                  from-orange-500
+                  to-orange-800
+                  text-gray-100
+                  
+              `}>
+                  {RenderHeader()}
+              </thead>
+              <tbody>
+                  {RenderData()}
+              </tbody>
+          </table>
+      )
+  }
+  
